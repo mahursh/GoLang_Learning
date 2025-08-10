b78fb77d908d47f8700131978230ffacbbb52fd9
Mahoor.Sh <mahurshams94@gmail.com>
2025-08-10 21:38:00 +0330

    leaning variables

--- GO projects/hello-world/main.go
+++ GO projects/hello-world/main.go
@@ -3,7 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	sayHelloWorld("Hello, World!")
+	//var   name   type
+	//var whatToSay string
+    //whatToSay = "Hello, World!"
+
+
+	// := is the short variable declaration operator that defines and initializes a variable in one step.
+	whatToSay := "Hello, World!"
+	sayHelloWorld(whatToSay)
 }
 
 func sayHelloWorld(whatToSay string){
