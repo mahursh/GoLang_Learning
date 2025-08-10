87cae5f5afa3dc51507e13e3af42c1a6ba14e77a
Mahoor.Sh <mahurshams94@gmail.com>
2025-08-10 23:21:43 +0330

    update

--- GO projects/hello-world/main.go
+++ GO projects/hello-world/main.go
@@ -1,18 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"myapp/doctor"
+)
 
 func main() {
-	//var   name   type
-	//var whatToSay string
-    //whatToSay = "Hello, World!"
-
-
-	// := is the short variable declaration operator that defines and initializes a variable in one step.
-	whatToSay := "Hello, World!"
-	sayHelloWorld(whatToSay)
-}
-
-func sayHelloWorld(whatToSay string){
+	whatToSay := doctor.Intro()
 	fmt.Println(whatToSay)
 }
+
