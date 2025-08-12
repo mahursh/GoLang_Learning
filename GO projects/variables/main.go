df3c1210aca5ea48b6585bae09cbf3f741154bfd
Mahoor.Sh <mahurshams94@gmail.com>
2025-08-12 19:33:13 +0330

    update

--- GO projects/variables/main.go
+++ GO projects/variables/main.go
@@ -3,17 +3,19 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math/rand/v2"
 	"os"
 )
 
 // constants never change
-const prompt = "and press ENTER when ready."
+const prompt = " don't type your number in, just press ENTER when ready."
 
 func main() {
-
-	var firstNumber = 2
-	var secondNumber = 5
-	var subtraction = 7
+	// seed the random number generator.
+	
+	var firstNumber = rand.IntN(8) + 2
+	var secondNumber = rand.IntN(8) + 2
+	var subtraction = rand.IntN(8) + 2
 	var answer int //becuase we did not gaved it a value, it will be asiigned with default value , for example for strings , its just a empty string.
 	reader := bufio.NewReader(os.Stdin)
 	// display a welcom/instructions
@@ -21,7 +23,7 @@ func main() {
 	fmt.Println("---------------------")
 	fmt.Println("")
 
-	fmt.Println("Think of a number between 1 ", prompt)
+	fmt.Println("Think of a number between 1 and 10 ", prompt)
 	reader.ReadString('\n')
 
 	//tgake them, through the game
