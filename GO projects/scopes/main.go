32c1c8cd2545753a906e4d8b4fa9869a6210456f
Mahoor.Sh <mahurshams94@gmail.com>
2025-08-14 13:53:17 +0330

    update

--- GO projects/scopes/main.go
+++ GO projects/scopes/main.go
@@ -11,7 +11,4 @@ func main() {
 	packageOne.PrintMe(myVar)
 	packageOne.PrintMe(packageOne.PackageVar)
 
-	//in the main function, print out the values of myVar,
-	//blockVar, and PackageVar on one line using the PrintMe  function in packageOne
-
 }
