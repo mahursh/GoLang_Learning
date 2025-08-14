9a664417ee19d893a3a8e581ecad53e458a42e21
Mahoor.Sh <mahurshams94@gmail.com>
2025-08-14 22:40:45 +0330

    update

--- GO projects/console-app/main.go
+++ GO projects/console-app/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"strconv"
 
 	"github.com/eiannone/keyboard"
 )
@@ -12,25 +13,35 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer keyboard.Close()
+
+	defer func() {
+		_ = keyboard.Close()
+	}()
+	fmt.Println("MENU")
+	fmt.Println("---")
+	fmt.Println("1 - Cappucino")
+	fmt.Println("2 - Latte")
+	fmt.Println("3 - Americano")
+	fmt.Println("4 - Mocha")
+	fmt.Println("5 - Macchiato")
+	fmt.Println("6 - Espresso")
+	fmt.Println("Q - Quite the program")
 
 	fmt.Println("Press any key on the keyboard. Press ESC to quit.")
 
 	for {
-		char, key, err := keyboard.GetSingleKey()
+		char, _, err := keyboard.GetSingleKey()
 		if err != nil {
 			log.Fatal(err)
 		}
-
-		if key == keyboard.KeyEsc {
+		//Runes (characters) %q
+		//
+		i, _ := strconv.Atoi((string(char)))
+		t := fmt.Sprintf("You chose %d", i)
+		
+		fmt.Println(t)
+		if char == 'q' || char == 'Q'{
 			break
-		}
-
-		// Handle printable keys
-		if key == 0 {
-			fmt.Printf("You pressed: %q\n", char)
-		} else {
-			fmt.Printf("You pressed special key: %v\n", key)
 		}
 	}
 
