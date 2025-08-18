b89607e71d5a38dc0e0f186bcd5e5cfa58998397
Mahoor.Sh <mahurshams94@gmail.com>
2025-08-18 21:19:40 +0330

    update

---
+++ GO projects/built-in-types-working/go.mod
@@ -0,0 +1,3 @@
+module builtInTypes
+
+go 1.24.6
---
+++ GO projects/built-in-types-working/main.go
@@ -0,0 +1,17 @@
+package main
+
+// basic types (numbers, strings, booleans)
+var myInt int
+var myInt16 int16
+var myInt32 int32
+var myInt64 int64
+
+
+// aggerigate types (array, struct)
+
+// reference types (pointers, slices, map, functions, channels)
+
+// interface type
+func main() {
+
+}
