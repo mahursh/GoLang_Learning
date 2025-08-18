e8982ebf0e298b4254c6d3fbf237b11395f630c9
Mahoor.Sh <mahurshams94@gmail.com>
2025-08-18 18:28:08 +0330

    update

--- GO projects/string-interpolation/main.go
+++ GO projects/string-interpolation/main.go
@@ -4,15 +4,18 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 var reader *bufio.Reader
 func main() {
 	reader = bufio.NewReader(os.Stdin)
 	userName := readString("What is your name?")
+
+	age := readInt("How old are you?")
 	
 
-	fmt.Println("Your name is", userName)
+	fmt.Println("Your name is", userName, " ,and you are ", age," years old.")
 }
 
 func prompt(){
@@ -24,6 +27,18 @@ func readString(s string) string{
 	prompt()
 	userInput, _ := reader.ReadString('\n')
 	userInput = strings.Replace(userInput, "\n", "", -1)
+	return userInput
+}
 
+func readInt(s string) int{
+	fmt.Println(s)
+	prompt()
+	userInput, _ := reader.ReadString('\n')
+	userInput = strings.Replace(userInput, "\n", "", -1)
+	num, err := strconv.Atoi(userInput)
+	if err != nil{
+		fmt.Println("Please enter a whole number.")
 
+	}
+	return num
 }
