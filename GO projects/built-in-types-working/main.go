64c34b0b46d68d064d9c178c4bfd54212d52f0b4
Mahoor.Sh <mahurshams94@gmail.com>
2025-08-20 18:30:43 +0330

    update

--- GO projects/built-in-types-working/main.go
+++ GO projects/built-in-types-working/main.go
@@ -1,11 +1,15 @@
 package main
 
+import "log"
+
 // basic types (numbers, strings, booleans)
 var myInt int
 var myInt16 int16
 var myInt32 int32
 var myInt64 int64
-
+var myUnit uint //only unsign integer .zero and positives
+var myFloat float32
+var myFloat64 float64
 
 // aggerigate types (array, struct)
 
@@ -14,4 +18,9 @@ var myInt64 int64
 // interface type
 func main() {
 
+	myInt = 10
+	myUnit = 20
+	myFloat = 10.1
+	myFloat = 100.1
+	log.Println(myInt,myUnit,myFloat,myFloat64)
 }
